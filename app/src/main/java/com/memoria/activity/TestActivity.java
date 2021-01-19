6c78d4ddf1b5599ad5652608e7ce841916fbb421
Kim-Jong-Gyu <62927374+Kim-Jong-Gyu@users.noreply.github.com>
2021-01-19 23:12:07 +0900

    ㅎㅎ
    DB 작업중

--- app/src/main/java/com/memoria/activity/TestActivity.java
+++ app/src/main/java/com/memoria/activity/TestActivity.java
@@ -1,6 +1,9 @@
 package com.memoria.activity;
 
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +15,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.memoria.R;
 import com.memoria.dbhelper.MyWordDBHelper;
 import com.memoria.modeldata.MyWord;
-
 
 
 import java.util.ArrayList;
@@ -27,39 +29,62 @@ public class TestActivity extends AppCompatActivity {
     TextView english;
     EditText answer;
     Button next;
+    MyWord currentWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        getList();
-        // 랜덤으로 섞는다.
-        Collections.shuffle(wordList);
-
-        english=findViewById(R.id.test_english);
-        answer=findViewById(R.id.test_answer);
-        next=findViewById(R.id.test_next);
-        for(MyWord element:wordList){
-            element.getEnglishWord();
-            english.setText((CharSequence) element);
-            next.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View view) {
-                    if(answer.getText().toString()==element.getKoreanWord().toString()){
-                        
-                    }
-                }
-            });
-        }
-    }
-
-    public void getList(){
         selectGroup = getIntent().getStringArrayListExtra("SELECTED_GROUP");
         myWordDBHelper = new MyWordDBHelper(this);
-        wordList= new ArrayList<MyWord>();
-        for(String element:selectGroup){
+        wordList = new ArrayList<MyWord>();
+        for (String element : selectGroup) {
             myWords = myWordDBHelper.selectWordListByGroup(element);
             wordList.addAll(myWords);
         }
+        // 랜덤으로 섞는다.
+        Collections.shuffle(wordList);
+        english = findViewById(R.id.test_english);
+        next = findViewById(R.id.test_next);
+        currentWord = wordList.get(0);
+        english.setText(currentWord.getEnglishWord());
+        answer = findViewById(R.id.test_answer);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (answer.getText().toString() == currentWord.getKoreanWord()) {
+                    if (wordList.size() == 1) {
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        wordList.remove(0);
+                        currentWord = wordList.get(0);
+                        english.setText(currentWord.getEnglishWord());
+                        answer.getText().clear();
+                        if(wordList.size()==1){
+                            next.setBackgroundColor(Color.parseColor("#7AEAC3"));
+                            next.setText("DONE");
+                        }
+                    }
+                } else {
+                    if (wordList.size() == 1) {
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        currentWord = wordList.get(1);
+                        wordList.remove(0);
+                        english.setText(currentWord.getEnglishWord());
+                        answer.getText().clear();
+                        if(wordList.size()==1){
+                            next.setBackgroundColor(Color.parseColor("#7AEAC3"));
+                            next.setText("DONE");
+                        }
+                    }
+                }
+            }
+
+        });
     }
 }
