54a4d5a4b5d568d3055ade02f14087d68b1bb9cf
Eunji <eunji>
2021-01-20 21:24:01 +0900

    // 백업복원 코드작성 중

--- app/src/main/java/com/memoria/activity/DataBackupActivity.java
+++ app/src/main/java/com/memoria/activity/DataBackupActivity.java
@@ -2,15 +2,183 @@ package com.memoria.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.memoria.R;
+import com.memoria.dbhelper.GoalDBHelper;
+import com.memoria.dbhelper.MyMemoryDBHelper;
+import com.memoria.dbhelper.MyWordDBHelper;
+import com.memoria.modeldata.Goal;
+import com.memoria.modeldata.MyMemory;
+import com.memoria.modeldata.MyWord;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class DataBackupActivity extends AppCompatActivity {
+
+    private EditText editmail;
+    private  EditText editpw;
+    private EditText checkpw;
+    private Button backupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_backup);
+
+        if(android.os.Build.VERSION.SDK_INT > 9){
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        editmail = findViewById(R.id.editmail);
+        editpw = findViewById(R.id.editpw);
+        checkpw = findViewById(R.id.checkpw);
+        backupButton = findViewById(R.id.backupButton);
+
+        backupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("클릭","클릭");
+                if (editpw.getText().toString().equals(checkpw.getText().toString())) {
+                    Log.d("editpw",editpw.getText().toString());
+                    Log.d("checkpw",checkpw.getText().toString());
+                    try {
+                        URL url = new URL("http://221.167.222.53:3001/data");
+                        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                        httpURLConnection.setDefaultUseCaches(false);
+                        httpURLConnection.setDoInput(true);
+                        httpURLConnection.setDoOutput(true);
+                        httpURLConnection.setRequestMethod("POST");
+                        httpURLConnection.setRequestProperty("Accept", "application/json");
+                        httpURLConnection.setRequestProperty("Content-type", "application/json");
+
+                        //보낼 JSONObject 생성
+                        JSONObject jsonObject = new JSONObject();
+                        JSONObject dataObject = new JSONObject();
+
+                        //Word
+                        JSONObject wordObject = new JSONObject();
+                        JSONArray wordArray = new JSONArray();
+                        MyWordDBHelper myWordDBHelper = new MyWordDBHelper(DataBackupActivity.this);
+                        ArrayList<MyWord> wordArrayList = myWordDBHelper.selectAllDataList();
+                        for (MyWord m : wordArrayList){
+                            wordObject.put("wGroupName",m.getGroupName());
+                            wordObject.put("wEnglishWord",m.getEnglishWord());
+                            wordObject.put("wKoreanWord",m.getKoreanWord());
+                            wordObject.put("wDate",m.getDate());
+                            wordArray.put(wordObject);
+                        }
+                        System.out.println(wordArray);
+
+                        //Memory
+                        JSONObject memoryObject = new JSONObject();
+                        JSONArray memoryArray = new JSONArray();
+                        MyMemoryDBHelper mymemoryDBHelper = new MyMemoryDBHelper(DataBackupActivity.this);
+                        ArrayList<MyMemory> mymemoryArrayList = mymemoryDBHelper.selectAllDataList();
+                        for (MyMemory m : mymemoryArrayList){
+                            memoryObject.put("mEnglishMemory",m.getEnglishMemory());
+                            memoryObject.put("mDate",m.getDate());
+                            memoryArray.put(memoryObject);
+                        }
+                        System.out.println(memoryArray);
+
+                        //Goal
+                        JSONObject goalObject = new JSONObject();
+                        JSONArray goalArray = new JSONArray();
+                        GoalDBHelper goalDBHelper = new GoalDBHelper(DataBackupActivity.this);
+                        ArrayList<Goal> goalArrayList = goalDBHelper.selectAllData();
+                        for (Goal g : goalArrayList){
+                            goalObject.put("gGoalWord",g.getGoalWord());
+                            goalObject.put("gGoalMemory",g.getGoalMemory());
+                            goalObject.put("gGoalTest",g.getGoalTest());
+                            goalObject.put("gGoalQuiz",g.getGoalQuiz());
+                            goalObject.put("gAchieveWord", g.getAchieveWord());
+                            goalObject.put("gAchieveMemory", g.getAchieveMemory());
+                            goalObject.put("gAchieveTest", g.getAchieveTest());
+                            goalObject.put("gAchieveQUiz", g.getAchieveQuiz());
+                            goalObject.put("gDate", g.getDate());
+                            goalArray.put(goalObject);
+                        }
+                        System.out.println(goalArray);
+
+                        dataObject.put("word",wordArray);
+                        dataObject.put("memory",memoryArray);
+                        dataObject.put("goal",goalArray);
+
+                        jsonObject.accumulate("password", editpw.getText().toString());
+                        jsonObject.accumulate("data", dataObject.toString());
+                        System.out.println(jsonObject);
+
+                        Log.d("성공3","성공3t");
+
+                        String json = jsonObject.toString();
+                        Log.d("성공4","성공4t");
+
+                        OutputStream os = null;
+                        os = httpURLConnection.getOutputStream();
+                        System.out.println(json);
+                        Log.d("성공41",os.toString());
+                        os.write(json.getBytes("euc-kr"));
+                        os.flush();
+                        os.close();
+                        Log.d("성공5", "성공5t");
+
+                        InputStream is = null;
+                        try{
+                            int reponseCode = httpURLConnection.getResponseCode();
+                            Log.d("서버 응답", Integer.toString(reponseCode));
+                            BufferedReader buf = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                            String inputLine;
+                            StringBuffer reponse = new StringBuffer();
+                            while ((inputLine = buf.readLine()) != null){
+                                reponse.append(inputLine);
+                            }
+                            buf.close();
+                            Log.d("Message:", reponse.toString());
+                        }finally {
+                            httpURLConnection.disconnect();
+                        }
+                    } catch (MalformedURLException e) {
+                            Log.d("MalformedURLException e","MalformedURLException e");
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            Log.d("IOException e","IOException e");
+                            e.printStackTrace();
+                        } catch (JSONException e) {
+                            Log.d("JSONException e","JSONException e");
+                            e.printStackTrace();
+                        }finally{
+                            Intent intent = new Intent(DataBackupActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(getApplicationContext(),"백업에 성공했습니다.",Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(),"비밀번호가 틀립니다.",Toast.LENGTH_LONG).show();
+                    }
+
+            }
+
+        });
     }
 }
--- app/src/main/java/com/memoria/activity/DataRestoreActivity.java
+++ app/src/main/java/com/memoria/activity/DataRestoreActivity.java
@@ -2,15 +2,143 @@ package com.memoria.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.memoria.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class DataRestoreActivity extends AppCompatActivity {
+
+    private Button restoreButton;
+    private EditText findpw;
+    private String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_restore);
+
+        if(android.os.Build.VERSION.SDK_INT > 9){
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        findpw = findViewById(R.id.findpw);
+        result = "";
+        restoreButton = findViewById(R.id.restoreButton);
+        restoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (findpw.getText().toString() != null){
+                    try {
+                        URL url = new URL("http://221.167.222.53:3001/data/" + findpw.getText().toString());
+                        System.out.println(findpw.getText().toString());
+                        System.out.println(url);
+                        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                        Log.d("ㅇ","ㅇ");
+
+                        httpURLConnection.setDefaultUseCaches(false);
+                        //httpURLConnection.setDoInput(true);
+                        //httpURLConnection.setDoOutput(true);
+                        httpURLConnection.setRequestMethod("GET");
+                        Log.d("ㅇ","ㅇ");
+
+                        InputStream is = httpURLConnection.getInputStream();
+                        StringBuilder sb = new StringBuilder();
+                        BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+                        String line;
+                        Log.d("ㅇ","ㅇ");
+
+                        while((line = br.readLine()) != null){
+                            sb.append(line);
+                        }
+                        Log.d("ㅇ","ㅇ");
+
+                        result = sb.toString();
+                        System.out.println(result);
+
+                        br.close();
+                        Log.d("ㅇ","ㅇ");
+
+                        addItem(result);
+                        Log.d("ㅇ","ㅇ");
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(result);
+                            JSONArray arr = jsonObject.getJSONArray("msg");
+                            Log.d("jsonfiletttt", arr.get(0).toString());
+                            String arr2 = arr.getString(0);
+                            Log.d("st1", arr2);
+                            JSONObject temp = new JSONObject(arr2);
+                            Log.d("st1", temp.toString());
+                            String arr3 = temp.getString("data");
+                            JSONObject temp2 = new JSONObject(arr3);
+
+                            Log.d("st2", arr3.toString() );
+                        } catch(JSONException error){
+                            Log.d("오류", error.toString() );
+                        }
+
+
+                    } catch (MalformedURLException e) {
+                        Log.d("MalformedURLException e", "MalformedURLException e");
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        Log.d("IOException e", "IOException e");
+                        e.printStackTrace();
+                    }finally{
+                        Intent intent = new Intent(DataRestoreActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(),"비밀번호를 입력하세요.",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void addItem(String result){
+        try {
+            JSONObject jsonObject = new JSONObject(result);
+            JSONArray msg = jsonObject.getJSONArray("msg");
+            Log.d("msg",msg.toString());
+
+            String data = msg.getString(0);
+            JSONObject dataObject = new JSONObject(data);
+            Log.d("data",dataObject.toString());
+
+            String menu = dataObject.getString("data");
+            JSONObject menuObject = new JSONObject(menu);
+            Log.d("menu",menuObject.toString());
+
+            String word = menuObject.getString("word");
+            JSONObject wordObject = new JSONObject(word);
+            Log.d("word",wordObject.toString());
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
